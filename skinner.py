a1d4606b37a99f904aa159ec7daa22ab405721cb
Michele Stofella <michele.stofella@studio.unibo.it>
2019-07-17 18:41:22 +0200

    Initial setup

---
+++ skinner.py
@@ -0,0 +1,57 @@
+# =============================================================================
+# 
+# Deuterium Uptake
+# Function to evaluate deuterium uptake is implemented
+# Evaluated deuterium uptake is shown in plot and compared with experimental data
+#
+# =============================================================================
+
+from skinner_setup import *
+
+# %%
+
+def deut_uptake(peptides, peptide_no, kint, pfact, t):
+    '''
+    Evaluates the deuterium uptake of a certain peptide
+    
+    Input parameters
+    ----------------
+    peptides: pandas dataframe, format ['peptide','start_res','stop_res']
+    peptide_no: number of the peptide to be analyzes, raw of the peptides dataframe
+    kint: pandas dataframe, format ['residue','k_int']
+    pfact: pandas dataframe containing protection factors, format ['residue','ln(p)']
+    t: time interval over which deuterium uptake should be evaluated
+    
+    Ouput
+    ----------------
+    deut_ptake: deuterium uptake calculated at time t
+    '''
+    j = peptide_no
+    start = peptides['start_res'][j]
+    stop = peptides['stop_res'][j]
+    
+    deut_partial = 0; namide = 0
+    for i in range(start, stop):
+        k_int = kint['k_int'][i]
+        p_fact = np.exp(pfact['ln(p)'][i])
+        k = 60*k_int/p_fact
+        if k >= 0:
+            namide += 1
+            deut_partial += np.exp(-k*t)
+    deut_uptake = (namide-deut_partial)/namide
+ 
+    return deut_uptake
+ 
+# %%
+
+for i in range(0,7):
+    plt.scatter(exp_data[0], exp_data[i+1])
+    plt.plot(time, deut_uptake(peptides, i, kint, pfact, time))
+
+plt.xlim(1e-6,1e4); plt.ylim(-0.1,1.1)
+plt.xlabel('Time (hours)',fontsize=18)
+plt.ylabel('Deuterium Uptake (D)', fontsize=18)
+plt.xscale('log')
+plt.grid(linestyle=':')
+
+# %%
---
+++ skinner_setup.py
@@ -0,0 +1,41 @@
+# =============================================================================
+# 
+# Reference data are here organised in pandas dataframes
+# Data are visualised
+# 
+# =============================================================================
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+pfact = pd.read_csv('test_data\\ref.pfact', sep=" ", header=None)
+pfact.columns = ["residue","ln(p)"]
+
+sequence = open('test_data\\test.seq','r')  
+if sequence.mode == 'r':
+    seq = sequence.read()    
+
+peptides = pd.read_csv('test_data\\test.list', sep=" ", header=None)
+peptides.columns = ["peptide","start_res","stop_res","sequence"]
+
+kint = pd.read_csv('test_data\\test.kint', header=None, sep='\t')
+kint.columns = ["residue","k_int"]
+# last value has been changed (it has been multiplied *100)
+
+exp_data = pd.read_csv('test_data\\test.Dexp', sep=" ", header=None)
+
+# %%
+
+""" This block is useful to visualize data """
+fig, (ax1,ax2) = plt.subplots(2, 1, figsize=[6, 7])
+
+ax1.plot(pfact["residue"],pfact["ln(p)"],'o-',color='black')
+ax1.grid(linestyle=':')
+
+for i in range(0,len(peptides["peptide"])):
+    ax2.hlines(y=peptides["peptide"][i], xmin=peptides["start_res"][i], xmax=peptides["stop_res"][i],
+               linestyles='-')
+ax2.grid(linestyle=':')
+
+# %%
